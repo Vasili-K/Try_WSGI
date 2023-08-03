474875ece355a6e8116408e7896fd9ac9157b9f8
Vasili-K <vasili5424999@gmail.com>
2023-08-03 14:53:13 +0200

    add descriotion

--- wsgi_web_app.py
+++ wsgi_web_app.py
@@ -1,7 +1,16 @@
 from wsgiref.simple_server import make_server
 
 
+# make_server creates a WSGI compliant server provided by Python
+
+
 def application(env, start_response):
+    """
+    This function presents a Web Application.
+    :param env: a variable containing various information about the request
+    :param start_response: is used to notify the server of the status of response and for setting various headers
+    :return: WSGI server expects the return from application to be an iterable
+    """
     path = env.get('PATH_INFO')
     if path == '/':
         response_body = b'This is the root url'
@@ -13,6 +22,11 @@ def application(env, start_response):
     start_response(status, response_headers)
     return [response_body]
 
+
+# httpd is the Web Server
+# The first and second argument to make_server specifies the host and port
+# on which the server will listen for requests
+# Third argument to make_server passes the Web Application which the Web Server would use to get the response
 
 httpd = make_server('127.0.0.1', 8051, application)
 
